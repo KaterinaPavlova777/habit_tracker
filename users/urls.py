00798c494c297715830a4d9a88277ca695dad8bb
KaterinaPavlova777 <miss.kat.katerina2007@gmail.com>
2025-06-05 09:57:52 +0300

    small fixes

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,6 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-
 from users.apps import UsersConfig
 from users.views import UserRegistration
 
@@ -12,6 +11,5 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
     path('register/', UserRegistration.as_view(), name='register'),
 ]
